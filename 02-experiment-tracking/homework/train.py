5733636221d2dc2b68d6ddb8a45eecc1f7f55749
Do Thanh Trung <trungthanhdo18102001@gmail.com>
2024-08-01 18:08:18 +0000

    update

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("nyc-taxi-experiment")
+mlflow.set_experiment("random-forest-train")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -24,21 +24,19 @@ def load_pickle(filename: str):
 
 
 def run_train(data_path: str):   
+    mlflow.sklearn.autolog()
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    with mlflow.start_run():
-        mlflow.set_tag("developer", "trungpy")
-        mlflow.log_param("train-data-path", "02-experiment-tracking/homework/output/train.pkl")
-        mlflow.log_param("valid-data-path", "02-experiment-tracking/homework/output/val.pkl")
-        mlflow.sklearn.autolog()
+    with mlflow.start_run():       
 
         rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
         rmse = root_mean_squared_error(y_val, y_pred)
+        print("RMSE: ", rmse)
     
 
 
---
+++ 02-experiment-tracking/note.md
@@ -0,0 +1,11 @@
+
+Parameter
+* 
+
+run MLflow
+```
+mlflow ui --backend-store-uri sqlite:///mlflow.db
+```
+
+Note:
+- Run mlflow trong cùng một folder với code log
